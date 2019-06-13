bbc235cb22475b675c636816b6a0770c42a5b7a9
numega0 <46091549+numega0@users.noreply.github.com>
2019-06-14 02:26:17 +0300

    Add files via upload

---
+++ DataAccess/EntityFramework/Repository/EfEntityRepositoryBase.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using Core.Entity;
+
+namespace Core.DataAccess.EntityFramework.Repository
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter, params string[] includeExpressions)
+        {
+            using (TContext context = new TContext())
+            {
+                IQueryable<TEntity> set = context.Set<TEntity>();
+
+                foreach (var includeExpression in includeExpressions)
+                {
+                    set = set.Include(includeExpression);
+                }
+
+                return set.SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null, string includes = null)
+        {
+            using (TContext context = new TContext())
+            {
+                if (includes != null)
+                    return filter == null ?
+                        context.Set<TEntity>().Include(includes).ToList() :
+                        context.Set<TEntity>().Include(includes).Where(filter).ToList();
+
+                return filter == null ?
+                    context.Set<TEntity>().ToList() :
+                    context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DataAccess/EntityFramework/Repository/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Core.Entity;
+
+namespace Core.DataAccess.EntityFramework.Repository
+{
+    public interface IEntityRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null, string includes = null);
+        TEntity Get(Expression<Func<TEntity, bool>> filter, params string[] includeExpressions);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+
+}
